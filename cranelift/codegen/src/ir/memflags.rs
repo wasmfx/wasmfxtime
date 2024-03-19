d2bfad041c5fb4459b3bf6d30cd9288f747660f1
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-03-19 10:49:08 +0100

    Fix memflags roundtrip test

--- cranelift/codegen/src/ir/memflags.rs
+++ cranelift/codegen/src/ir/memflags.rs
@@ -338,7 +338,7 @@ impl MemFlags {
             0b1001 => Some(TrapCode::UnreachableCodeReached),
             0b1010 => Some(TrapCode::Interrupt),
             0b1011 => Some(TrapCode::NullReference),
-            // 0b1100 => {} not allocated
+            0b1100 => Some(TrapCode::UnhandledTag),
             // 0b1101 => {} not allocated
             // 0b1110 => {} not allocated
             0b1111 => None,
