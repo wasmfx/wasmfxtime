4896c7fdc37f5305a85caaa02c3445028a2480f0
yinamy <yinamy@outlook.com>
2026-04-02 13:56:06 +0100

    Fixed things that failed CI checks

--- crates/wasmtime/src/runtime/vm/instance/allocator/wasmfx_allocator.rs
+++ crates/wasmtime/src/runtime/vm/instance/allocator/wasmfx_allocator.rs
@@ -91,7 +91,7 @@ pub mod wasmfx_pooling {
         pub fn new(config: &WasmFXConfig) -> Result<Self> {
             use rustix::mm::{mprotect, MprotectFlags};
 
-            let total_stacks : u32 = config.total_stacks as u32; /* total amount of stacks */
+            let total_stacks: u32 = config.total_stacks.try_into()?; /* total amount of stacks */
 
             if total_stacks == 0 {
                 bail!("Total_stacks value is zero in pooling allocator");
